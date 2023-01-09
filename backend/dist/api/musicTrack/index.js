c090780c42a0821b04ce67634b831c217c3c5a3a
Quincin <119814773+Quincin2000@users.noreply.github.com>
2023-01-09 18:36:59 +0600

    Add files via upload

---
+++ backend/dist/api/musicTrack/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (app) => {
+    app.post(`/tenant/:tenantId/music-track`, require('./musicTrackCreate').default);
+    app.put(`/tenant/:tenantId/music-track/:id`, require('./musicTrackUpdate').default);
+    app.post(`/tenant/:tenantId/music-track/import`, require('./musicTrackImport').default);
+    app.delete(`/tenant/:tenantId/music-track`, require('./musicTrackDestroy').default);
+    app.get(`/tenant/:tenantId/music-track/autocomplete`, require('./musicTrackAutocomplete').default);
+    app.get(`/tenant/:tenantId/music-track`, require('./musicTrackList').default);
+    app.get(`/tenant/:tenantId/music-track/:id`, require('./musicTrackFind').default);
+    app.get(`/radioList`, require('./radioList').default);
+};
+//# sourceMappingURL=index.js.map
